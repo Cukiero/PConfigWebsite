f4c236b4fa85bf82192383729a7d81289299ea8e
Danalscy <danalan97@gmail.com>
2018-01-19 23:39:52 +0100

    Add files via upload
    Jeszcze Raz Final Test

--- PConfigAuth/Controllers/ConfigureController.cs
+++ PConfigAuth/Controllers/ConfigureController.cs
@@ -476,28 +476,6 @@ namespace PConfigAuth.Controllers
             return await LoadDataFromHwType(hwtype);
         }
 
-        public void AddConfiguration(CPU cpu, GPU gpu, MOBO mobo, RAM ram, PSU psu, Case casePC, Cooler cooler, ApplicationUser user)
-        {
-            PC pcdata = new PC();
-            pcdata.CPU = cpu;
-            pcdata.GPU = gpu;
-            pcdata.MOBO = mobo;
-            pcdata.RAM = ram;
-            pcdata.PSU = psu;
-            pcdata.Case = casePC;
-            pcdata.Cooler = cooler;
-            pcdata.ApplicationUser = user;
-            _context.PCs.Add(pcdata);
-            _context.SaveChanges();
-        }
-
-        public List<PC> GetPCs()
-        {
-            var query = from b in _context.PCs
-                        orderby b.Id
-                        select b;
-
-            return query.ToList();
-        }
+       
     }
 }
